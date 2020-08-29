d3f59a3ae5dce95b57c792b6327d5277dce7f9e9
Arkadiusz Kruszyński <arkadiusz.h.kruszynski@gmail.com>
2020-08-29 09:16:00 +0200

    no message

--- Apka/app/src/main/java/com/example/asystentoro/MainActivity.kt
+++ Apka/app/src/main/java/com/example/asystentoro/MainActivity.kt
@@ -71,7 +71,7 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present. mmm
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
